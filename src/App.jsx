1555554ebe0f31ef3592afb1cbd0b3e741e219d3
unknown <adhikariroshun@gmail.com>
2025-07-19 09:01:39 +0545

    just learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Navbar from "./Navbar";
 function App() {
   return(
     <>
     <h1>
       BAKAS
       </h1>
+      <Navbar />
       </>
   )
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar(){
+    return(
+        <>
+        <nav className="div">
+            <a href="/">Home</a>
+            <a href="/">About</a>
+            <a href="/">Contact</a>
+            <a href="/">Login</a>
+            <a href="/">Signup</a>
+            <a href="/">Logout</a>
+            <a href="/">Profile</a>
+            <a href="/">Settings</a>
+        </nav>
+        </>
+)}
+export default Navbar;
